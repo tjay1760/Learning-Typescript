9f99ae222ee4adddee00d40716a1c3d28d1c55cd
tjay1760 <mcjthiongo@gmail.com>
2025-06-16 12:15:07 +0300

    init file

---
+++ index.js
@@ -0,0 +1,16 @@
+const menu = [
+    { name: "Margherita Pizza", price: 8.99 },
+    { name: "Cheeseburger", price: 10.49 },
+    { name: "Caesar Salad", price: 7.25 },
+    { name: "Spaghetti Bolognese", price: 11.50 },
+    { name: "Grilled Chicken Sandwich", price: 9.75 },
+    { name: "Fish and Chips", price: 12.00 },
+    { name: "Veggie Wrap", price: 8.50 },
+    { name: "Steak Frites", price: 15.99 },
+    { name: "Chicken Tikka Masala", price: 13.25 },
+    { name: "Chocolate Lava Cake", price: 6.75 }
+];
+
+const cashInRegister = 1000;
+
+const orderQueue = [];
