363c01a4c0e7197b2ec0ba876ed7838ba798a41c
tjay1760 <mcjthiongo@gmail.com>
2025-06-18 14:20:57 +0300

    fixed routes

--- backend/JSMastery-Backend-ApI/app.js
+++ backend/JSMastery-Backend-ApI/app.js
@@ -1,12 +1,16 @@
 import express from "express"
 import { PORT } from "./config/env.js";
-
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import authRouter from "./routes/auth.routes.js";
 const app = express();
-
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users',userRouter);
+app.use('/api/v1/subscriptions',subscriptionRouter)
 app.get("/",(req,res)=>{
 res.send("Welcome to the subscription tracker API")
 })
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log(`app running on port http://localhost:${PORT}`)
 })
 export default app;
--- backend/JSMastery-Backend-ApI/routes/subscription.route.js
+++ backend/JSMastery-Backend-ApI/routes/subscription.route.js
@@ -2,10 +2,28 @@ import { Router } from "express";
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req,res)=>{
-res.send("GET all subscriptions")
+res.send({title:"GET all subscriptions"})
 })
-subscriptionRouter.get('/', (req,res)=>{
-res.send("GET all subscriptions")
+subscriptionRouter.get('/id', (req,res)=>{
+res.send({title:"GET a subscription"})
+})
+subscriptionRouter.post('/', (req,res)=>{
+res.post({title:"CREATE a subscription"})
+})
+subscriptionRouter.put(':/id', (req,res)=>{
+res.send({title:"UPDATE a subscription"})
+})
+subscriptionRouter.delete('/:id', (req,res)=>{
+res.send({title: "DELETE a subscription"})
+})
+subscriptionRouter.get('/user/:id', (req,res)=>{
+res.send( {title:"GET a users subscriptions"})
+})
+subscriptionRouter.put('/:id/cancel', (req,res)=>{
+res.send({title: "CANCEL a users subscription"})
+})
+subscriptionRouter.get('/upcoming-renewals', (req,res)=>{
+res.send({title: "GET upcoming renewals"})
 })
 
 export default subscriptionRouter;
