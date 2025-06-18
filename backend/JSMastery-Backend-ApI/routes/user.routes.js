b02691791db210f954be5f04decf297e74273de4
tjay1760 <mcjthiongo@gmail.com>
2025-06-18 12:26:31 +0300

    user routes

---
+++ backend/JSMastery-Backend-ApI/routes/user.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get("/",(req,res)=>{
+    res.send({title:"GET all users"})
+})
+userRouter.get("/:id",(req,res)=>{
+    res.send({title:"GET user details"})
+})
+userRouter.post("/",(req,res)=>{
+    res.send({title:"ADD new users"})
+})
+userRouter.put("/:id",(req,res)=>{
+    res.send({title:"UPDATE a user"})
+})
+userRouter.delete("/:id",(req,res)=>{
+    res.send({title:"DELETE a user"})
+})
